6447f5f5bc1e74b89bf67c0643d6cd7128f32338
mtnmr <mei.rila1013@gmail.com>
2022-09-23 16:33:19 +0900

    media3 exoplayer

--- exoplayer-intro-main/exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ exoplayer-intro-main/exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -16,13 +16,17 @@
 package com.example.exoplayer
 
 import android.os.Bundle
-import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
 import com.example.exoplayer.databinding.ActivityPlayerBinding
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.util.Util
+
 
 /**
  * A fullscreen activity to play audio or video streams.
@@ -33,36 +37,73 @@ class PlayerActivity : AppCompatActivity() {
         ActivityPlayerBinding.inflate(layoutInflater)
     }
 
-    private var player:ExoPlayer? = null
+    private var player: ExoPlayer? = null
+
+    private var playWhenReady = true
+    private var currentItem = 0
+    private var playbackPosition = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
     }
 
-    //API レベル 24 以上の Android は、複数のウィンドウをサポートします。
-    //アプリは視認可能ですが、分割ウィンドウ モードではアクティブにならないため、onStart でプレーヤーを初期化する必要があります。
+    //APIレベル24以上のAndroid は、複数のウィンドウをサポートする。アプリは視認可能だが、分割ウィンドウモードではアクティブにならないため、onStartでプレーヤーを初期化する
     override fun onStart() {
         super.onStart()
 
-        if(Util.SDK_INT >= 24){
+        if(Util.SDK_INT > 23){
             initializePlayer()
         }
     }
 
-    //API レベル 24 以下の Android では、アプリがリソースを取得するまでできるだけ長く待機する必要があるため、
-    //プレーヤーを初期化する前に onResume まで待ちます。
+    //APIレベル24以下のAndroid では、アプリがリソースを取得するまでできるだけ長く待機する必要があるため、プレーヤーを初期化する前に onResume まで待つ。
     override fun onResume() {
         super.onResume()
 
         hideSystemUi()
-        if ((Util.SDK_INT < 24 || player == null)) {
+        if ((Util.SDK_INT <= 23 || player == null)) {
             initializePlayer()
         }
     }
 
+    //全画面表示するために他のUIを非表示する
     private fun hideSystemUi() {
-        
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, viewBinding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    //APIレベル 24 以下では、onStop が呼び出される保証がないため、onPause でできるだけ早くプレーヤーを解放する
+    override fun onPause() {
+        super.onPause()
+
+        if(Util.SDK_INT <= 23){
+            releasePlayer()
+        }
+    }
+
+    //APIレベル24以上（マルチウィンドウモードと分割ウィンドウモードが導入されている）では、onStopが呼び出されることが保証される。
+    // 一時停止状態では、アクティビティが引き続き表示されるため、onStopまで待ってからプレーヤーを解放する
+    override fun onStop() {
+        super.onStop()
+
+        if(Util.SDK_INT > 23){
+            releasePlayer()
+        }
+    }
+
+    private fun releasePlayer() {
+        //playerを破棄する前に情報を保存しておき、中断したところから再開できるようにする
+        player?.let { exoPlayer ->
+            playbackPosition = exoPlayer.contentPosition
+            currentItem = exoPlayer.currentMediaItemIndex
+            playWhenReady = exoPlayer.playWhenReady
+            exoPlayer.release()
+        }
+        player = null
     }
 
     private fun initializePlayer(){
@@ -72,6 +113,9 @@ class PlayerActivity : AppCompatActivity() {
                 viewBinding.videoView.player = exoPlayer
                 val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp3))
                 exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.playWhenReady = playWhenReady
+                exoPlayer.seekTo(currentItem, playbackPosition)
+                exoPlayer.prepare()
             }
     }
 }
